279c93b06607ee19dba6ebc72dff901fb90ebac3
webdizz <webdizz@gmail.com>
2010-11-07 15:21:46 +0200

    Extension name was changed #terciman

--- src/main/java/name/webdizz/clt/crx/client/BackgroundPage.java
+++ src/main/java/name/webdizz/clt/crx/client/BackgroundPage.java
@@ -11,12 +11,12 @@ import com.mvp4g.client.Mvp4gModule;
 /**
  * The Chrome extension background page script.
  */
-@Extension.ManifestInfo(name = "Loogat (by Izzet Mustafaiev)", description = "Qırımtatarian-Russian and Russian-Qırımtatarian translator.", version = BackgroundPage.VERSION, permissions = {
+@Extension.ManifestInfo(name = "Terciman (by Izzet Mustafa)", description = "The Google Chrome extension to translate selected word to/from Crimean Tatar language", version = BackgroundPage.VERSION, permissions = {
 		"tabs", "http://qlugat.appspot.com/lugat/get*" }, icons = { "resources/icon16.png", "resources/icon32.png",
 		"resources/icon48.png", "resources/icon128.png" })
 public class BackgroundPage extends Extension {
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	static Console CONSOLE = Chrome.getExtension().getBackgroundPage().getConsole();
 
