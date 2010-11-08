c03cdf1ce8ab38941c8e60c8d405f6c419952cad
webdizz <webdizz@gmail.com>
2010-11-09 00:44:45 +0200

    Add abilities to log to console #terciman

--- src/main/java/name/webdizz/clt/crx/client/presenter/MonitorPresenter.java
+++ src/main/java/name/webdizz/clt/crx/client/presenter/MonitorPresenter.java
@@ -6,6 +6,8 @@ package name.webdizz.clt.crx.client.presenter;
 import name.webdizz.clt.crx.client.Alert;
 import name.webdizz.clt.crx.client.ExtConfiguration;
 
+import com.google.gwt.chrome.crx.client.Chrome;
+import com.google.gwt.chrome.crx.client.Console;
 import com.google.inject.Inject;
 import com.mvp4g.client.annotation.Presenter;
 import com.mvp4g.client.event.EventBus;
@@ -21,22 +23,34 @@ public class MonitorPresenter extends
 
 	interface IMonitorPresenterView {
 	}
-	
+
 	@Inject
 	private ExtConfiguration configuration;
+
+	private Console console = Chrome.getExtension().getBackgroundPage()
+			.getConsole();
 
 	public static class MonitorPresenterView implements IMonitorPresenterView {
 	}
 
 	public void onError(String message) {
-		if(configuration.isDebug()) Alert.info("Error: " + message);
+		if (configuration.isDebug())
+			Alert.info("Error: " + message);
+		if (!configuration.isDebug())
+			console.log("Error: " + message);
 	}
 
 	public void onInfo(String message) {
-		if(configuration.isDebug()) Alert.info("Info: " + message);
+		if (configuration.isDebug())
+			Alert.info("Info: " + message);
+		if (!configuration.isDebug())
+			console.log("Info: " + message);
 	}
-	
+
 	public void onTrace(String message) {
-		if(configuration.isDebug()) Alert.info("Trace: " + message);
+		if (configuration.isDebug())
+			Alert.info("Trace: " + message);
+		if (!configuration.isDebug())
+			console.log("Trace: " + message);
 	}
 }
